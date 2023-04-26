1b5ef0818c2a2f9592df6a21b52bc605f69c83c5
Maxence Guidez <maxence.guigui@gmail.com>
2023-04-26 16:00:05 -0400

    Space laboratory, make a margin around the grid.

--- src/main/java/org/calma/pig/exercices/laboSpacial/SpaceGridApplication.java
+++ src/main/java/org/calma/pig/exercices/laboSpacial/SpaceGridApplication.java
@@ -1,6 +1,7 @@
 package org.calma.pig.exercices.laboSpacial;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -9,6 +10,7 @@ import javafx.stage.Stage;
 import net.kurobako.gesturefx.GesturePane;
 import org.calma.pig.exercices.laboSpacial.repositories.obstacle.IObstacleRepository;
 import org.calma.pig.exercices.laboSpacial.repositories.obstacle.InMemoryObstacleRepository;
+import org.calma.pig.utils.BorderUtils;
 
 import java.io.IOException;
 
@@ -20,20 +22,13 @@ public class SpaceGridApplication extends Application {
 
         SpaceGrid grilleCanvas = null;
         try {
-            grilleCanvas = new SpaceGrid(emplacementRepository, 200, 200);
+            grilleCanvas = new SpaceGrid(emplacementRepository, 100, 100);
         }
         catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        StackPane stackPane = new StackPane();
-        stackPane.getChildren().add(grilleCanvas);
-
-        StackPane.setAlignment(grilleCanvas, Pos.CENTER);
-
-        GesturePane gp = new GesturePane(stackPane);
-
-        BorderPane root = new BorderPane(gp);
+        BorderPane root = initRoot(grilleCanvas, false);
 
         Scene scene = new Scene(root, 960, 540);
         primaryStage.setScene(scene);
@@ -44,6 +39,30 @@ public class SpaceGridApplication extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public BorderPane initRoot(SpaceGrid spaceGrid, boolean withGesture){
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(spaceGrid);
+
+        StackPane.setAlignment(spaceGrid, Pos.CENTER);
+
+        BorderUtils borderU = new BorderUtils();
+        stackPane.setBorder(borderU.createBorder());
+
+        BorderPane root = null;
+        if(withGesture){
+            GesturePane gp = new GesturePane(stackPane);
+
+            root = new BorderPane(gp);
+        }
+        else{
+            root = new BorderPane(stackPane);
+        }
+
+        root.setPadding(new Insets(80.0, 40.0, 20.0, 40.0));
+
+        return root;
     }
 }
 
